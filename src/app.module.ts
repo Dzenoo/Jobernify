6c0c4076b284f0864d4e0b800cb402cadb5b510b
Dzenoo <dzenisgudzevic18@gmail.com>
2024-11-17 18:28:24 +0100

    Generating resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { AppService } from './app.service';
 
 import { AppConfigModule } from './config/app/config.module';
 import { MongooseConfigModule } from './config/database/mongodb/config.module';
+import { ApplicationsModule } from './models/applications/applications.module';
+import { EmployersModule } from './models/employers/employers.module';
+import { JobsModule } from './models/jobs/jobs.module';
+import { ReviewsModule } from './models/reviews/reviews.module';
+import { SeekersModule } from './models/seekers/seekers.module';
 
 @Module({
-  imports: [AppConfigModule, MongooseConfigModule],
+  imports: [AppConfigModule, MongooseConfigModule, ApplicationsModule, EmployersModule, JobsModule, ReviewsModule, SeekersModule],
   controllers: [AppController],
   providers: [AppService],
 })
