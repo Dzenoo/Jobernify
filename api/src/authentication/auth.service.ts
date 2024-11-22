fca6dbfa1291cb83046b52ea403e391c81a5d27c
Dzenoo <dzenisgudzevic18@gmail.com>
2024-11-22 16:36:11 +0100

    Refactoring auth signup logic

--- api/src/authentication/auth.service.ts
+++ api/src/authentication/auth.service.ts
@@ -70,27 +70,18 @@ export class AuthService {
     };
   }
 
-  async signupSeeker(body: SignupSeekerDto) {
-    const existingEmployer = await this.employersService.findOneByEmail(
-      body.email,
-    );
+  async signup(
+    body: SignupSeekerDto | SignUpEmployerDto,
+    userType: 'seeker' | 'employer',
+  ) {
+    const emailCheckResult = await this.checkEmailExistence(body.email);
 
-    if (existingEmployer) {
-      throw new ConflictException(
-        'This email is already associated with one account',
-      );
-    }
-
-    const existingSeeker = await this.seekersService.findOneByEmail(body.email);
-
-    if (existingSeeker) {
-      const isEmailVerified = existingSeeker.emailVerified;
-
-      const errorMessage = isEmailVerified
+    if (emailCheckResult) {
+      const errorMessage = emailCheckResult.emailVerified
         ? 'An account with this email already exists.'
         : 'An account with this email already exists but is not verified. Please check your email for the verification link or request a new one.';
 
-      throw isEmailVerified
+      throw emailCheckResult.emailVerified
         ? new ConflictException(errorMessage)
         : new NotAcceptableException(errorMessage);
     }
@@ -98,30 +89,36 @@ export class AuthService {
     const { verificationToken, verificationExpiration } =
       this.verificationService.create();
 
-    const newSeeker = await this.seekersService.createOne({
-      ...body,
-      verificationToken,
-      verificationExpiration,
-    });
+    let newUser;
 
-    if (!newSeeker) {
+    if (userType === 'seeker') {
+      const seekerBody = {
+        ...(body as SignupSeekerDto),
+        verificationToken,
+        verificationExpiration,
+      };
+
+      newUser = await this.seekersService.createOne(seekerBody);
+    } else {
+      const employerBody = {
+        ...(body as SignUpEmployerDto),
+        verificationToken,
+        verificationExpiration,
+      };
+
+      newUser = await this.employersService.createOne(employerBody);
+    }
+
+    if (!newUser) {
       throw new InternalServerErrorException(
         'Cannot register account, please try again',
       );
     }
 
-    const emailContent = `
-        <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
-          <h2 style="color: #333;">Jobernify - Verify your email</h2>
-          <p style="color: #555;">Please verify your email by clicking on this link: <a href="${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&type=seeker" style="color: #1a73e8;">Verify Email</a></p>
-          <p style="color: #555;">This token expires in 24 hours, so please verify your account within this timeframe.</p>
-        </div>
-      `;
-
-    await this.emailService.sendMail(
-      newSeeker.email,
-      'Jobernify - Verify your email',
-      emailContent,
+    await this.sendVerificationEmail(
+      newUser.email,
+      verificationToken,
+      userType,
     );
 
     return {
@@ -130,63 +127,53 @@ export class AuthService {
     };
   }
 
+  async signupSeeker(body: SignupSeekerDto) {
+    return this.signup(body, 'seeker');
+  }
+
   async signupEmployer(body: SignUpEmployerDto) {
-    const existingSeeker = await this.seekersService.findOneByEmail(body.email);
+    return this.signup(body, 'employer');
+  }
 
+  private async checkEmailExistence(email: string) {
+    const existingSeeker = await this.seekersService.findOneByEmail(email);
     if (existingSeeker) {
-      throw new ConflictException(
-        'This email is already associated with one account',
-      );
+      return {
+        userType: 'seeker',
+        emailVerified: existingSeeker.emailVerified,
+      };
     }
 
-    const existingEmployer = await this.employersService.findOneByEmail(
-      body.email,
-    );
-
+    const existingEmployer = await this.employersService.findOneByEmail(email);
     if (existingEmployer) {
-      const isEmailVerified = existingEmployer.emailVerified;
-
-      const errorMessage = isEmailVerified
-        ? 'An account with this email already exists.'
-        : 'An account with this email already exists but is not verified. Please check your email for the verification link or request a new one.';
-
-      throw isEmailVerified
-        ? new ConflictException(errorMessage)
-        : new NotAcceptableException(errorMessage);
+      return {
+        userType: 'employer',
+        emailVerified: existingEmployer.emailVerified,
+      };
     }
 
-    const { verificationToken, verificationExpiration } =
-      this.verificationService.create();
+    return null;
+  }
 
-    const newEmployer = await this.employersService.createOne({
-      ...body,
-      verificationToken,
-      verificationExpiration,
-    });
-
-    if (!newEmployer) {
-      throw new InternalServerErrorException(
-        'Cannot register account, please try again',
-      );
-    }
-
+  private async sendVerificationEmail(
+    email: string,
+    verificationToken: string,
+    userType: string,
+  ) {
     const emailContent = `
-        <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
-          <h2 style="color: #333;">Jobernify - Verify your email</h2>
-          <p style="color: #555;">Please verify your email by clicking on this link: <a href="${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&type=seeker" style="color: #1a73e8;">Verify Email</a></p>
-          <p style="color: #555;">This token expires in 24 hours, so please verify your account within this timeframe.</p>
-        </div>
-      `;
+                          <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
+                            <h2 style="color: #333;">Jobernify - Verify your email</h2>
+                            <p style="color: #555;">Please verify your email by clicking on this link: 
+                            <a href="${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&type=${userType}" 
+                            style="color: #1a73e8;">Verify Email</a></p>
+                            <p style="color: #555;">This token expires in 24 hours, so please verify your account within this timeframe.</p>
+                          </div>
+                         `;
 
     await this.emailService.sendMail(
-      newEmployer.email,
+      email,
       'Jobernify - Verify your email',
       emailContent,
     );
-
-    return {
-      statusCode: HttpStatus.CREATED,
-      message: 'Signup successful! Please verify your email.',
-    };
   }
 }
