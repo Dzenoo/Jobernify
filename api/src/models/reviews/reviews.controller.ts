17d8c7041690fcc16e4dddc93cd88a2e13d9eddc
Dzenoo <dzenisgudzevic18@gmail.com>
2024-11-23 12:58:15 +0100

    Add Review

--- api/src/models/reviews/reviews.controller.ts
+++ api/src/models/reviews/reviews.controller.ts
@@ -1,19 +1,40 @@
-import { Controller, Post, Patch, Delete, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 
 import { ReviewsService } from './reviews.service';
 
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 
+import { User } from 'src/common/decorators/user.decorator';
+
+import { JwtAuthGuard } from 'src/authentication/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/authentication/guards/role-auth.guard';
+
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '../shared/schemas/user.schema';
+
 @Controller('/reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
   @Post('/:employerId')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Seeker)
   async create(
+    @User('userId') userId: string,
     @Body() body: CreateReviewDto,
     @Param('employerId') employerId: string,
-  ) {}
+  ) {
+    return await this.reviewsService.createOne(userId, employerId, body);
+  }
 
   @Patch('/:reviewId')
   async edit(
--- api/src/models/reviews/reviews.service.ts
+++ api/src/models/reviews/reviews.service.ts
@@ -1,13 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  forwardRef,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Review } from './schemas/review.schema';
 
 import { Model } from 'mongoose';
 
+import { CreateReviewDto } from './dto/create-review.dto';
+
+import { EmployersService } from '../employers/employers.service';
+import { SeekersService } from '../seekers/seekers.service';
+
 @Injectable()
 export class ReviewsService {
   constructor(
-    @InjectModel(Review.name) private readonly reviewModel: Model<Review>,
+    @Inject(forwardRef(() => EmployersService))
+    private readonly employersService: EmployersService,
+    @Inject(forwardRef(() => SeekersService))
+    private readonly seekersService: SeekersService,
+    @InjectModel(Review.name)
+    private readonly reviewModel: Model<Review>,
   ) {}
+
+  async createOne(
+    seekerId: string,
+    employerId: string,
+    body: CreateReviewDto,
+  ): Promise<ResponseObject> {
+    const employer = await this.employersService.findOneById(employerId);
+
+    if (!employer) {
+      throw new NotFoundException('The specified employer could not be found.');
+    }
+
+    const existingReview = await this.reviewModel.findOne({
+      seeker: seekerId,
+      company: employerId,
+    });
+
+    if (existingReview) {
+      throw new ForbiddenException(
+        'You have already submitted a review for this employer.',
+      );
+    }
+
+    const review = await this.reviewModel.create({
+      ...body,
+      company: employerId,
+      seeker: seekerId,
+    });
+
+    await this.employersService.findOneByIdAndUpdate(employerId, {
+      $push: {
+        reviews: review._id,
+      },
+    });
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Review successfully added',
+    };
+  }
+
+  async editOne(): Promise<ResponseObject> {
+    return {
+      statusCode: HttpStatus.OK,
+    };
+  }
+
+  async deleteOne(): Promise<ResponseObject> {
+    return {
+      statusCode: HttpStatus.OK,
+    };
+  }
 }
