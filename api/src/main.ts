a2945d23f5885dfa9d3b62ad9c025c7fdef2c0fc
Dzenoo <dzenisgudzevic18@gmail.com>
2024-11-25 14:18:17 +0100

    Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,8 +9,7 @@ async function initializeServer() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    // origin: ['https://jobernify.com', 'http://localhost:3000'],
+    origin: ['https://jobernify.com', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
