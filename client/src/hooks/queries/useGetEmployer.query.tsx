fb74610f6bb33d0da3ba2c34e6183df859cc57dc
Dzenoo <dzenisgudzevic18@gmail.com>
2024-12-05 20:13:59 +0100

    Refactor useGetEmployer query to utilize useSuspenseQuery for improved data fetching

--- client/src/hooks/queries/useGetEmployer.query.tsx
+++ client/src/hooks/queries/useGetEmployer.query.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 
 import { getEmployerProfile } from "@/lib/actions/employers.actions";
 
@@ -7,14 +7,14 @@ import useAuthentication from "../defaults/useAuthentication.hook";
 const useGetEmployer = (type?: string) => {
   const { token } = useAuthentication().getCookieHandler();
 
-  return useQuery({
+  return useSuspenseQuery({
     queryFn: () =>
       getEmployerProfile({
         token: token as string,
         type: type,
       }),
     queryKey: ["profile"],
-    enabled: !!token,
+    // enabled: !!token,
   });
 };
 
