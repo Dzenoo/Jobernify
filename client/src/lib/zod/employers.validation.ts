3da0c6d1bd5dbaa008eef5a3cb7000e658d9e869
Dzenoo <dzenisgudzevic18@gmail.com>
2024-12-16 15:56:00 +0100

    Fixing zod again

--- client/src/lib/zod/employers.validation.ts
+++ client/src/lib/zod/employers.validation.ts
@@ -1,14 +1,14 @@
 import zod from 'zod';
 
-import { CompanySize, IndustryType } from '@jobernify/shared';
+import { companySizes, industries } from '@/constants';
 
 export const EmployerProfileSchema = zod.object({
   name: zod.string().min(5).max(50),
   address: zod.string().min(5).max(50),
-  industry: zod.nativeEnum(IndustryType, {
+  industry: zod.enum(industries, {
     message: 'Please select valid industry',
   }),
   website: zod.string().max(30),
-  size: zod.nativeEnum(CompanySize, { message: 'Please select valid size' }),
+  size: zod.enum(companySizes, { message: 'Please select valid size' }),
   companyDescription: zod.string().max(300),
 });
