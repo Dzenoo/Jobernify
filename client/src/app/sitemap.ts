230b0e18071c0e917c2c38f7318a799abcad09fa
Dzenoo <dzenisgudzevic18@gmail.com>
2025-01-12 10:54:32 +0100

    Updating robots and sitemap

--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -4,18 +4,51 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${process.env.NEXT_PUBLIC_CLIENT_URL}`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'daily',
+      priority: 1.0,
     },
     {
       url: `${process.env.NEXT_PUBLIC_CLIENT_URL}about-us`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     {
       url: `${process.env.NEXT_PUBLIC_CLIENT_URL}faq`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     {
       url: `${process.env.NEXT_PUBLIC_CLIENT_URL}seeker-guidelines`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
     },
     {
       url: `${process.env.NEXT_PUBLIC_CLIENT_URL}employer-guidelines`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}terms-and-conditions`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}privacy-policy`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_CLIENT_URL}cookie-policy`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
     },
   ];
 }
