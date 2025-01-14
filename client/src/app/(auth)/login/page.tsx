9c279b85ee181896ec283a7e3693ec0adacba72f
Dzenoo <dzenisgudzevic18@gmail.com>
2025-01-14 08:33:04 +0100

    Fixing error

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,19 +1,21 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 import Login from '@/components/auth/login/Login';
 
 import { useToast } from '@/components/ui/info/use-toast';
 
-export default function LoginPage() {
+const LoginPage = ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) => {
   const [errorMessage, setErrorMessage] = useState('');
-  const searchParams = useSearchParams();
   const { toast } = useToast();
 
   useEffect(() => {
-    const error = searchParams.get('error');
+    const error = searchParams.error;
     if (error) {
       setErrorMessage(error);
     }
@@ -34,4 +36,6 @@ export default function LoginPage() {
       <Login />
     </section>
   );
-}
+};
+
+export default LoginPage;
